081e54204eedb31d4c45de5c7324408d8073d587
larbi <hm_larbi@esi.dz>
2023-12-17 18:51:44 +0100

    tutorial section

--- packages/client/src/components/Tutorial/Tutorial.tsx
+++ packages/client/src/components/Tutorial/Tutorial.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 import { useTranslation } from "react-i18next";
 import { SharedLayout } from "scenes/Menu";
 import './style.css';
-// const logoIcp = require("./images/logo-icp.jpg");
+const join = require("./images/joinp.png");
 // const logoFsm = require("./images/logo-fsm.jpg");
 // const logoLp = require("./images/logo-lp.png");
 // const logoBlog = require("./images/logo_espectateur.png");
@@ -24,7 +24,7 @@ export const Tutorial: React.FC = () => {
       >
         <Typography align='center' variant="h2" gutterBottom={true}     style={{
                 margin: '40px',
-                color: '#0B9A8D',
+                color:'#6aa84f',
               }}>
           Comment utiliser l'outil E-spectateur?
         </Typography>
@@ -33,9 +33,24 @@ export const Tutorial: React.FC = () => {
         Avec E-spect@teur, vous pouvez être créateur de projet ou simplement participer à un projet existant.
 
         </Typography>
-        <div className="box">
-      {/* Content of the box goes here */}
-    </div>
+        <div className="spacer"></div>
+        <div className="vertical-container">
+         <div className="vertical-box">
+            <p className="box-text">Comment rejoindre E-specta@teur ?</p>
+          <div className="box-column">
+              <img src={join} alt="Erasmus"/>
+          </div>
+           <div className="box-column">
+              <img src={join} alt="Erasmus"/>
+        
+          </div>
+          </div>
+
+
+          <div className="vertical-box">Box 2</div>
+          <div  className="vertical-box">Box 3</div>
+
+        </div>
       
       </div>
     </SharedLayout>
